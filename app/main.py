f28f7196847eb7186c583ac34758326b94c6d9c1
Batsandi <geol.aleksandar@gmail.com>
2025-04-18 11:16:37 +0300

    basic fastapi routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
-
+from pydantic import HttpUrl
 app = FastAPI(title="Collector Service")
+
 
 @app.get("/isalive")
 async def is_alive():
-  return {"status": "alive"}
+    return {"status": "alive"}
+
+
+@app.post("/screenshots")
+async def capture_screenshots(start_url: HttpUrl, n_links: int):
+    return {"status": f"{str(start_url)}"}
+
+
+@app.get("/screenshot/{run_id}")
+async def return_screenshots(run_id: str):
+    return {"status": f"Screenshot for run ID {run_id}"}
