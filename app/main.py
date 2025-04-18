f12011b4a9e428a627744eb98f2de379d17eead0
Batsandi <geol.aleksandar@gmail.com>
2025-04-18 11:54:52 +0300

    cleaned up routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
+from typing import List, Optional
 from fastapi import FastAPI
-from pydantic import HttpUrl
+from pydantic import HttpUrl, BaseModel
+from random import randint
+
 app = FastAPI(title="Collector Service")
+
+
+class CaptureScreenshotRequest(BaseModel):
+    start_url: HttpUrl
+    n_links: int
+
+
+class CaptureScreenshotResponse(BaseModel):
+    run_id: str
+
+
+class ReturnScreenshotResponse(BaseModel):
+    run_id: str
+    status: str
+    screenshots: Optional[List[str]]
+
+
+class StatusResponse(BaseModel):
+    status: str
 
 
 @app.get("/isalive")
@@ -10,9 +32,14 @@ async def is_alive():
 
 @app.post("/screenshots")
 async def capture_screenshots(start_url: HttpUrl, n_links: int):
-    return {"status": f"{str(start_url)}"}
+    # TODO: use uuid for example to generate run_ids
+    # Perfectly aware this is not a robust solution
+    # I chose this for simplicity to have a simple input for the GET route
+    run_id = randint(1000, 9999)
+
+    return {"run_id": f"{run_id}"}
 
 
 @app.get("/screenshot/{run_id}")
 async def return_screenshots(run_id: str):
-    return {"status": f"Screenshot for run ID {run_id}"}
+    return {"screenshots": ['list of screenshots']}
